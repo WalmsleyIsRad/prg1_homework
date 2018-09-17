22d193e1995b6032bc226b3baad17ce90134e59f
WalmsleyIsRad <31770493+WalmsleyIsRad@users.noreply.github.com>
2018-09-16 20:56:24 -0500

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,1 +1,25 @@
+'''#problem 1
 
+i = 16
+fp = 1989.69
+wow = "Why dis assignment Mr. Gold?"
+b = i==fp
+print(b, fp, i, wow)
+'''
+
+
+'''
+#problem 2
+print("Equilateral Triangle Formula Example")
+a = 4
+area = ((3**(1/2))/4) * (a**2)
+print(area)
+
+print("Square Formula Example")
+area = (a**2)
+print(area)
+
+print("Pentagon Formula Example")
+area =  (1/4) * ((5 * (5 + (2 * (5**(1/2)))))**(1/2)) * (a**2)
+print(area)
+'''
